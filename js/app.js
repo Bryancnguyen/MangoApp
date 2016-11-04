4cd34c91e74c26e7fcb8c04f3f2e8cdab11d519a
bryancnguyen <bryancanhnguyen@gmail.com>
2016-11-03 17:31:13 -0700

    Adding multiple HTML pages and comments for frontpage.css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-var mainApp = angular.module('myApp', []);
+var mainApp = angular.module('myApp', ['ngRoute']);
 
 mainApp.controller('mainController', ['$scope', function($scope)
 {
     $scope.name = 'YAY';
 }]);
+
+mainApp.config(function($routeProvider) {
+  $routeProvider
+  .get("/", {
+    templateUrl : "index.html"
+  })
+  .get("/slice", {
+    templateUrl : "slice.html"
+  })
+  .when("/login", {
+    templateUrl : "login.html"
+  })
+  .when("/register", {
+    templateUrl : "register.html"
+  })
+    .when("/lucky", {
+    templateUrl : "lucky.html"
+  })
+    .when("/404",
+    {
+        templateUrl: "404.html"
+    });
+});
